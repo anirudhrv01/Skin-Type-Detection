1f639a46484580d3538abffc7928b235247c6c68
anirudhrv01 <anirudhrv6@gmail.com>
2026-02-19 20:48:08 +0530

    Test 2

---
+++ models/test.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+from .resnet_model import SkinTypeResNet
+
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model=SkinTypeResNet(num_classes=4)
+model.load_state_dict(torch.load("models/best_model.pth"))
+model.to(device)
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+image_path = "data/single/DrySkin.jpg"
+image = Image.open(image_path).convert("RGB")
+image = transform(image).unsqueeze(0).to(device)
+
+with torch.no_grad():
+    outputs = model(image)
+    _, predicted = torch.max(outputs, 1)
+
+classes = ['combination', 'dry', 'normal', 'oily']
+
+print("Predicted Skin Type:", classes[predicted.item()])
